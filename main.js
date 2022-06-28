7a7f7f2621fc3e073ddbce35674a6747888dd3f4
Naman Rai <namanrai306@gmail.com>
2022-06-29 01:23:21 +0530

    more changes to landing page + svg animation

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+//title animation
+const title = document.querySelectorAll("#title1 path");
+
+for (let i = 0; i < title.length; i++){
+    console.log (`Letter ${i} is ${title[i].getTotalLength()}`);
+}
