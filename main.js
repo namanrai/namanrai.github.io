dfc26a504428f7ffb2f7c40e8e65bbe722e8ac03
Naman Rai <namanrai306@gmail.com>
2023-05-25 00:58:06 -0400

    added hacker text

--- main.js
+++ main.js
@@ -6,32 +6,32 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
-//title animation
-const title = document.querySelectorAll("#title1 path");
+// //title animation
+// const title = document.querySelectorAll("#title1 path");
 
-for (let i = 0; i < title.length; i++){
-    console.log (`Letter ${i} is ${title[i].getTotalLength()}`);
-}
+// for (let i = 0; i < title.length; i++){
+//     console.log (`Letter ${i} is ${title[i].getTotalLength()}`);
+// }
 
-/*scroll symbol*/
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
+// /*scroll symbol*/
+// function reveal() {
+//     var reveals = document.querySelectorAll(".reveal");
+//     for (var i = 0; i < reveals.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = reveals[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
+//       if (elementTop < windowHeight - elementVisible) {
+//         reveals[i].classList.add("active");
+//       } else {
+//         reveals[i].classList.remove("active");
+//       }
+//     }
+//   }
 
-  window.addEventListener("scroll", reveal);
+//   window.addEventListener("scroll", reveal);
 
-// To check the scroll position on page load
-reveal();
+// // To check the scroll position on page load
+// reveal();
 
 
 //background hover
@@ -48,3 +48,44 @@ document.body.onpointermove = event => {
   
 
 }
+
+
+//Text effect
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let intervals = [];
+
+function applyTextEffect(element) {
+  let iteration = 0;
+  clearInterval(intervals[element.dataset.index]);
+
+  intervals[element.dataset.index] = setInterval(() => {
+    element.innerText = element.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return element.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= element.dataset.value.length) {
+      clearInterval(intervals[element.dataset.index]);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+}
+
+window.onload = () => {
+  document.querySelectorAll(".texteffect").forEach((element, index) => {
+    element.dataset.index = index;
+    applyTextEffect(element);
+  });
+};
+
+document.querySelectorAll(".texteffect").forEach(element => {
+  element.addEventListener("mouseover", event => {
+    applyTextEffect(event.target);
+  });
+});
